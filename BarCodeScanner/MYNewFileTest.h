586a9eab9bc45874572a3666ea8a636eba7c8a13
reubro <reubro@reubros-imac.reubro.in>
2016-01-13 15:27:27 +0530

    Created New Class for Testing new Branches

---
+++ BarCodeScanner/MYNewFileTest.h
@@ -0,0 +1,13 @@
+//
+//  MYNewFileTest.h
+//  BarCodeScanner
+//
+//  Created by reubro on 13/01/16.
+//  Copyright © 2016 reubro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYNewFileTest : NSObject
+
+@end
---
+++ BarCodeScanner/MYNewFileTest.m
@@ -0,0 +1,13 @@
+//
+//  MYNewFileTest.m
+//  BarCodeScanner
+//
+//  Created by reubro on 13/01/16.
+//  Copyright © 2016 reubro. All rights reserved.
+//
+
+#import "MYNewFileTest.h"
+
+@implementation MYNewFileTest
+
+@end
